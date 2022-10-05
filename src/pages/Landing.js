c8874c5a4749e1e4db1e4be95dc6e7b549890e61
Zayd Nubani <zayd.nubani@gmail.com>
2022-10-05 10:56:06 -0400

    landing sizing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,10 +3,10 @@ const Landing = () => {
 
     return (
         <div className="d-flex flex-column">
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/about">About</a>
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/portfolio">Portfolio</a>
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/experience">Experience</a>
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/music">Music</a>    
+            <a className='futura text-decoration-none mx-2 my-1' style={{color: 'white', fontSize: '10vh'}} href="/about">About</a>
+            <a className='futura text-decoration-none mx-2 my-1' style={{color: 'white', fontSize: '10vh'}} href="/portfolio">Portfolio</a>
+            <a className='futura text-decoration-none mx-2 my-1' style={{color: 'white', fontSize: '10vh'}} href="/experience">Experience</a>
+            <a className='futura text-decoration-none mx-2 my-1' style={{color: 'white', fontSize: '10vh'}} href="/music">Music</a>    
         </div>
     )
 }
