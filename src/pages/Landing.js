5c25555d5d5ece752bc5143abb3597329650484b
Zayd Nubani <zayd.nubani@gmail.com>
2022-10-05 10:40:20 -0400

    stuff

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,10 +3,10 @@ const Landing = () => {
 
     return (
         <div className="d-flex flex-column">
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vw'}} href="/about">About</a>
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vw'}} href="/portfolio">Portfolio</a>
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vw'}} href="/experience">Experience</a>
-            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vw'}} href="/music">Music</a>    
+            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/about">About</a>
+            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/portfolio">Portfolio</a>
+            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/experience">Experience</a>
+            <a className='futura text-decoration-none mx-3' style={{color: 'white', fontSize: '10vh'}} href="/music">Music</a>    
         </div>
     )
 }
