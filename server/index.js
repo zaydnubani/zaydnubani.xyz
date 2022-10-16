c9c507c8ddf9942b05d15d28e4631010fed71bbb
Zayd Nubani <zayd.nubani@gmail.com>
2022-10-15 23:32:04 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -106,7 +106,7 @@ app.post('/api/contact', async (req, res) => {
 
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'))
 })
 
 // This creates ruum_db if it doesn't exist and connects with it
