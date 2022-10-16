23be4f7f9a40ef5fd7ebe25baa1c3e4f189950b3
Zayd Nubani <zayd.nubani@gmail.com>
2022-10-16 18:51:41 -0400

    about update

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -26,34 +26,31 @@ const About = () => {
             src: mirage
         },
         {
-            where: 'Salt Lake City, Utah',
-            src: mountain
-        },
-        {
             where: 'Akurreyri, Iceland',
             src: grass
         }
     ]
 
     return (
-        <div className='d-flex flex-row flex-wrap p-5'>
-            <div  className='d-flex flex-column align-items-center -justify-content-evenly' style={{width: '18rem'}}>
-                <span className='futura fs-2 my-3' style={{color: 'white'}}>Hello, I'm Zayd!</span>
-                <span className='futura fs-5 my-4' style={{color: 'white'}}>
-                    I'm a full-stack developer. When I'm not engulfed by my work, I'm off adventuring; wether that be near or far, the adventurers life is for me! Here are a few photos I've taken on my adventures in the past year that I really liked.
-                </span>
+        <div className='d-flex flex-row flex-wrap'>
+            <div className='card m-2' style={{width: '18rem'}}>
+                <div className='card-body d-flex flex-column align-items-center justify-content-evenly'>
+                    <span className='card-title futura fs-1' style={{color: '#006994'}}>Hello there.</span>
+                    <span className='card-subtitle futura fs-5' style={{color: '#006994'}}>My name is Zayd, I am a full-stack developer. When I'm not working, I'm off on an adventure whether that be near or far; for you to get a better idea of what I've been doing, here are some pictures I've taken while traveling.</span>
+                </div>
             </div>
             {
                 travels.map((res)=>{
                     return(
-                        <div className='m-1 d-flex flex-column align-items-center p-3' key={travels.indexOf(res)} style={{width: '18rem'}}>
-                            <label style={{color: 'white'}} className='fs-5 futura'>{res.where}</label>
-                            <img className="rounded m-1 img-fluid" src={res.src} alt='...'/>
+                        <div className='card m-2' style={{width: '18rem'}} key={travels.indexOf(res)}>
+                            <div className='card-body d-flex align-items-center justify-content-center'>
+                                <span className='card-title futura fs-5' style={{color: '#006994'}}>{res.where}</span>
+                            </div>
+                            <img className='card-img-top' alt='...' src={res.src}/>
                         </div>
                     )
                 })
             }
-
         </div>
     )
 }
